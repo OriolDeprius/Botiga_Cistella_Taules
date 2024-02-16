76128bfbb1fba55a80863a70b5f56207d7d59b57
OriolCASA <a2207810@institutmontilivi.cat>
2024-02-16 16:55:42 +0100

    Acabat per part d'Oriol

--- Botiga_Cistella_Taules/Program.cs
+++ Botiga_Cistella_Taules/Program.cs
@@ -7,12 +7,15 @@
             string[] productesBotiga = new string[] { "kebab", "cocacola", "pomes", "peres", "llimones", "platans", "arros", "", "", "" };
             double[] preus = new double[] { 3.5, 2, 2, 3, 4, 5, 6, 0, 0, 0 };
             int nElemBotiga = 7;
+            int li = 0;
+            int ls = nElemBotiga - 1;
 
             string[] productesCistella = new string[10];
             int[] productes = new int[10];
             int nElemCistella = productesCistella.Length;
             double diners;
 
+            Console.Clear();
             switch (Menu())
             {
                 case 1:
@@ -45,10 +48,17 @@
                     ModificarProducte(productesBotiga, producteAntic, producteNou);
                     break;
                 case 6:
+                    OrdenarProducte(productesBotiga, preus, li, ls);
+                    Console.Clear();
+                    Mostrar(productesBotiga, preus, nElemBotiga);
                     break;
                 case 7:
+                    OrdenarPreus(productesBotiga, preus, li, ls);
+                    Console.Clear();
+                    Mostrar(productesBotiga, preus, nElemBotiga);
                     break;
                 case 8:
+                    Mostrar(productesBotiga, preus, nElemBotiga);
                     break;
                 case 9:
                     break;
@@ -60,47 +70,43 @@
                     break;
                 case 13:
                     break;
-                case 14:
-                    break;
 
             }
         }
         static int Menu()
         {
             int opcio;
-            Console.WriteLine("╔═══════════════════════╗\n" +
-                              "║---------MENÚ----------║\n" +
-                              "╠═══════════════════════╣\n" +
-                              "║********BOTIGA*********║\n" +
-                              "╠═══════════════════════╣\n" +
-                              "║1. Afegir 1 Producte   ║\n" +
-                              "║2. Afegir +1 Producte  ║\n" +
-                              "║3. Ampliar Botiga      ║\n" +
-                              "║4. Modificar Preu      ║\n" +
-                              "║5. Modificar Producte  ║\n" +
-                              "║6. Ordenar Producte    ║\n" +
-                              "║7. Ordenar Preus       ║\n" +
-                              "║8. Mostrar             ║\n" +
-                              "║9. Mostrar Tot         ║\n" +
-                              "╠═══════════════════════╣\n" +
-                              "║*******CISTELLA********║\n" +
-                              "╠═══════════════════════╣\n" +
-                              "║10. Comprar 1 Producte ║\n" +
-                              "║11. Comprar +1 Producte║\n" +
-                              "║12. Ordenar Cistella   ║\n" +
-                              "║13. Mostra             ║\n" +
-                              "║14. Mostra Tot         ║\n" +
-                              "╚═══════════════════════╝\n");
+            Console.WriteLine("╔════════════════════════╗\n" +
+                              "║----------MENÚ----------║\n" +
+                              "╠════════════════════════╣\n" +
+                              "║*********BOTIGA*********║\n" +
+                              "╠════════════════════════╣\n" +
+                              "║ 1. Afegir 1 Producte   ║\n" +
+                              "║ 2. Afegir +1 Producte  ║\n" +
+                              "║ 3. Ampliar Botiga      ║\n" +
+                              "║ 4. Modificar Preu      ║\n" +
+                              "║ 5. Modificar Producte  ║\n" +
+                              "║ 6. Ordenar Producte    ║\n" +
+                              "║ 7. Ordenar Preus       ║\n" +
+                              "║ 8. Mostrar             ║\n" +
+                              "╠════════════════════════╣\n" +
+                              "║********CISTELLA********║\n" +
+                              "╠════════════════════════╣\n" +
+                              "║ 9. Comprar 1 Producte  ║\n" +
+                              "║ 10. Comprar +1 Producte║\n" +
+                              "║ 11. Ordenar Cistella   ║\n" +
+                              "║ 12. Mostra             ║\n" +
+                              "║ 13. Mostra Tot         ║\n" +
+                              "╚════════════════════════╝\n");
             Console.Write("Tria una opcio: ");
             opcio = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
             return opcio;
         }
         static void AfegirProducte(string[] productesBotiga, double[] preus, ref int nElemBotiga)
         {
             string producteAfegir;
             double preuAfegir;
-            
+
             if (productesBotiga.Length == nElemBotiga)
             {
                 char resposta;
@@ -175,7 +181,7 @@
         static void ModificarPreu(string[] productesBotiga, double[] preus, string producteModificar)
         {
             int posicio = -1;
-            
+
             for (int i = 0; i < productesBotiga.Length; i++)
             {
                 if (producteModificar == productesBotiga[i])
@@ -212,6 +218,53 @@
             {
                 productesBotiga[posicio] = producteNou;
             }
+        }
+        static void Mostrar(string[] productesBotiga, double[] preus, int nElem)
+        {
+            for (int i = 0; i < nElem; i++)
+                Console.WriteLine(BotigaToString(productesBotiga, preus, nElem, i));
+        }
+        static string BotigaToString(string[] productesBotiga, double[] preus, int nElem, int i)
+        {
+            return $"Producte: {productesBotiga[i]} | Preu: {preus[i]};";
+        }
+        static void OrdenarPreus(string[] producteBotiga, double[] preus, int li, int ls)
+        {
+            int mig = Convert.ToInt32(preus[(li + ls) / 2]);
+            int i = li;
+            int j = ls;
+            do
+            {
+                while (preus[i] < mig && i < ls) i++;
+                while (preus[j] > mig && j > li) j--;
+                if (i <= j)
+                {
+                    (preus[i], preus[j]) = (preus[j], preus[i]);
+                    (producteBotiga[i], producteBotiga[j]) = (producteBotiga[j], producteBotiga[i]);
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (j > li) OrdenarPreus(producteBotiga, preus, li, j);
+            if (i < ls) OrdenarPreus(producteBotiga, preus, i, ls);
+        }
+        static void OrdenarProducte(string[] producteBotiga, double[] preus, int li, int ls)
+        {
+            string mig = producteBotiga[(li + ls) / 2];
+            int i = li;
+            int j = ls;
+            do
+            {
+                while (producteBotiga[i].CompareTo(mig) < 0 && i < ls) i++;
+                while (producteBotiga[j].CompareTo(mig) > 0 && j > li) j--;
+                if (i <= j)
+                {
+                    (preus[i], preus[j]) = (preus[j], preus[i]);
+                    (producteBotiga[i], producteBotiga[j]) = (producteBotiga[j], producteBotiga[i]);
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Botiga_Cistella_Taules
- 
+
+Aquest programa consisteix en un petit menu on es simula el funcionament d'una petita botiga (molt per sobre).
+
+El funiconament es molt senzill i entenedor. Només has de indicar quina opció vols córrer indicant el número que hi ha a l'esquerra.
